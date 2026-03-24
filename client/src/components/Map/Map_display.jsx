3c4fcbad604439740c142c938cc9cdc843d1bcba
Aurelia Weickgenannt <aurelia.weickgenannt@students.fhnw.ch>
2026-03-24 12:13:02 +0100

    Diagramm_erstellen
    Daten für Diagramme hinzugefügt und die Dateien für die Diagramme erstellt. Daten in die Files importiert

--- client/src/components/Map/Map_display.jsx
+++ client/src/components/Map/Map_display.jsx
@@ -5,7 +5,7 @@ import "./Map_display.css";
 function Map() {
   return (
     <map>
-      <Box sx={{width: 1400, height: 700, backgroundColor: "yellow", border: "1px dashed black"}}> Karte Platzhalter</Box>    
+      <Box sx={{width: 1000, height: 500, backgroundColor: "yellow", border: "1px dashed black"}}> Karte Platzhalter</Box>    
     </map>
   );
 }
