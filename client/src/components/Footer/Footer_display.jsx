440c7fc9f6c77cd8a010c1734d38217a44e2925d
Aurelia Weickgenannt <aurelia.weickgenannt@students.fhnw.ch>
2026-04-20 15:32:03 +0200

    Kantone als WMS vom Geoserver eingebunden

--- client/src/components/Footer/Footer_display.jsx
+++ client/src/components/Footer/Footer_display.jsx
@@ -9,11 +9,21 @@ import "./Footer_display.css";
 function Footer( {value, setValue, playing, setPlaying}) {
   // Slider
   const marks = [
-  {value: 0, label: '',},
-  {value: 25, label: '',},
-  {value: 50, label: '',},
-  {value: 75, label: '',},
-  {value: 1556, label: '',},];
+  {value: 1, label: 'Start der Datenaufzeichnungen',},
+  {value: 25, label: '25.02.2020',},
+  {value: 28, label: '28.02.2020',},
+  {value: 42, label: '13.03.2020',},
+  {value: 45, label: '16.03.2020',},
+  {value: 87, label: '27.04.2020',},
+  {value: 101, label: '11.05.2020',},
+  {value: 157, label: '06.07.2020',},
+  {value: 293, label: '19.11.2020',},
+  {value: 327, label: '23.12.2020',},
+  {value: 353, label: '18.01.2021',},
+  {value: 591, label: '13.09.2021',},
+  {value: 689, label: '20.12.2021',},
+  {value: 1556, label: 'Ende der letzten Datenaufzeichnungen',},
+];
   
   return (
     <footer >
@@ -28,7 +38,7 @@ function Footer( {value, setValue, playing, setPlaying}) {
           <Box className="slider">
             <Slider
               aria-label="Custom marks"
-              min={0}
+              min={1}
               max={1556}
               value={value}
               onChange={(e, newValue) => setValue(newValue)}
--- client/src/components/Map/Map_display.jsx
+++ client/src/components/Map/Map_display.jsx
@@ -14,49 +14,50 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
+import TileLayer from "ol/layer/Tile.js";
+import TileWMS from "ol/source/TileWMS.js"
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4.js";
+
 import "./Map_display.css";
 
 function MapDisplay() {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    const vectorSource = new VectorSource({
-      url: "/kantone.geojson",
-      format: new GeoJSON({
-        dataProjection: "EPSG:4326",
-        featureProjection: "EPSG:3857"}),
-    });
 
-    const vectorLayer = new VectorLayer({
-      source: vectorSource,
-      style: new Style({
-        fill: new Fill({
-          color: "rgba(0, 100, 255, 0.3)",
-        }),
-        stroke: new Stroke({
-          color: "#0033aa",
-          width: 1.5,
-        }),
+    proj4.defs(
+      "EPSG:2056",
+      "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs"
+    );
+    register(proj4);
+
+    const wmsLayer = new TileLayer({
+      source: new TileWMS({
+        url: "http://localhost:8080/geoserver/CoronaDashboard/wms",
+        params: {
+          LAYERS: "CoronaDashboard:kantonsflaechen",
+          TILED: true,
+        },
+        serverType: "geoserver",
+        crossOrigin: "anonymous"
       }),
-    });
+    })
 
     const map = new Map({
       target: mapRef.current,
-      layers: [vectorLayer],
+      layers: [wmsLayer],
       view: new View({
-        center:[0, 0], 
-        zoom: 2
+        projection: "EPSG:2056",
+        center:[2659632, 1191208], 
+        zoom: 1
       }),
     });
 
-    vectorSource.on("change", () => {
-      if (vectorSource.getState() === "ready") {
-        map.getView().fit(vectorSource.getExtent(), {
-          padding: [20, 20, 20, 20],
-          maxZoom: 12,
-        });
-      }
-    });
+    map.getView().fit(
+      [2485410.0, 1075268.125, 2833857.75, 1295933.75],
+      {padding: [20, 20, 20, 20],}
+    );
 
     return () => {
       map.setTarget(null);
@@ -65,26 +66,10 @@ function MapDisplay() {
 
   // Infos Schweiz Box
     const cards = [
-      {
-        id: 1,
-        title: 'Totale Anschteckungen:',
-        description: 'Wert',
-      },
-      {
-        id: 2,
-        title: 'Tägliche Neuansteckungen:',
-        description: 'Wert',
-      },
-      {
-        id: 3,
-        title: 'Totale Todesfälle:',
-        description: 'Wert',
-      },
-      {
-        id: 4,
-        title: 'Totale Hospitalisierungen:',
-        description: 'Wert',
-      },
+      {id: 1, title: 'Totale Anschteckungen:', description: 'Wert',},
+      {id: 2, title: 'Tägliche Neuansteckungen:', description: 'Wert',},
+      {id: 3, title: 'Totale Todesfälle:', description: 'Wert',},
+      {id: 4, title: 'Totale Hospitalisierungen:', description: 'Wert',},
     ];
 
   return (
